9bb2a67930f1f0020f6a7e6d9fe723de7929cca5
adelina010416 <adelinazima@mail.ru>
2023-06-28 14:21:50 +0400

    home_work_4

---
+++ src/video.py
@@ -0,0 +1,26 @@
+import os
+
+from googleapiclient.discovery import build
+
+
+class Video:
+    api_key: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, video_id):
+        self.video_id = video_id
+        video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                     id=video_id).execute()
+        self.title = video_response['items'][0]['snippet']['title']
+        self.url = f'https://youtu.be/{video_id}'
+        self.view_count = video_response['items'][0]['statistics']['viewCount']
+        self.like_count = video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return self.title
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.pl_id = playlist_id
