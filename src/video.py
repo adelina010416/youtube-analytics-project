cfbe6dc0bcab325af7a61a04f9c5c13a0ad2694d
adelina010416 <adelinazima@mail.ru>
2023-07-01 17:25:20 +0400

    home_work_6

--- src/video.py
+++ src/video.py
@@ -12,10 +12,16 @@ class Video:
         self.video_id = video_id
         video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                      id=video_id).execute()
-        self.title = video_response['items'][0]['snippet']['title']
-        self.url = f'https://youtu.be/{video_id}'
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.title = video_response['items'][0]['snippet']['title']
+            self.url = f'https://youtu.be/{video_id}'
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.title
@@ -25,3 +31,7 @@ class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.pl_id = playlist_id
+
+
+v = Video('her')
+
